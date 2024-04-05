a4b49f594a39e6ac9e25fc3545219628a691fe2f
Octave Muhirwa <octy40@gmail.com>
2024-04-05 07:54:31 -0400

    first commit

---
+++ tests/integration/components/nav-bar-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'super-rentals/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | nav-bar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<NavBar />`);
+
+    assert.dom('nav').exists();
+    assert.dom('.menu .menu-index').hasText('SuperRentals');
+    assert.dom('.menu .menu-about').hasText('About');
+    assert.dom('.menu .menu-contact').hasText('Contact');
+  });
+});
